7ec9713994e1217c8eaca0a39eac00dbf22029b4
reksar2012 <stenchev60@gmail.com>
2018-12-10 12:20:26 +0300

    добавил ещё элементов

--- app/main.js
+++ app/main.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-    $('.fixed-action-btn').floatingActionButton();
+    $('.fixed-action-btn').floatingActionButton();    
+    $('.dropdown-trigger').dropdown();
+    $('.sidenav').sidenav();
   });
